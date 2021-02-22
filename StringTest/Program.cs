d8999fe5574653e232f4c006f11435e543104d04
noodle333 <oliver.nordin@elev.ga.ntig.se>
2021-02-22 10:30:50 +0100

    final comitt

--- StringTest/Program.cs
+++ StringTest/Program.cs
@@ -8,6 +8,22 @@ namespace StringTest
         {
             string[] highScores = {"LENA 100000", "MICKE 23004", "MARTIN 2000"};
             int result = 0;
+            int userPoint = 0;
+
+            Console.WriteLine("Skriv in ett användarnamn.");
+            string userName = Console.ReadLine().ToUpper();
+            while (userPoint <= 0)
+            {
+                Console.WriteLine("Hej, " + userName + ". Skriv in ett positivt tal som representerar dina poäng.");
+                int.TryParse(Console.ReadLine(), out userPoint);
+                if(userPoint <= 0)
+                {
+                    Console.WriteLine("Inkorrekt input, skriv in ett positivt tal.");
+                }
+            }
+            
+            userName = userName + " " + userPoint;
+            highScores[2] = userName;
 
             for (int i = 0; i < highScores.Length; i++)
             {
@@ -28,7 +44,6 @@ namespace StringTest
                 }
             }
             Console.WriteLine("Medelvärdet av poängen är " + result/highScores.Length);
-
             Console.ReadLine();
         }
     }
